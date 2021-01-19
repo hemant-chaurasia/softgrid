418fb551899128f6b8c6d21ab136b5b57fb1d393
hemant-chaurasia <hemantchaurasia002@gmail.com>
2021-01-19 19:59:39 +0530

    this is my first commit by hemant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, HostListener, Inject } from '@angular/core';
+import { MyserviceService } from './myservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  preserveWhitespaces:false
 })
 export class AppComponent {
   title = 'firstapp';
+  name = 'Hemant';
+  // constructor(@Inject(MyserviceService)myserviceService){
+  //   console.log(myserviceService);
+  //   console.log("hi this is component ");
+  // }
+  // @HostListener('click',['$eve'])
+  // onhostclick(event:Event){
+  //   alert("on click" );
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { CompanyModule } from './company/company.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CompanyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(){
+    console.log("hi this is module ");
+  }
+ }
